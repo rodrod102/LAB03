47a450a61e36e958b83d7390c2568fc91a0e52cb
rodrod102 <s5935512059@gmail.com>
2019-09-21 22:32:27 +0700

    finish

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,10 +1,28 @@
-import React ,{Component} from 'react'
-export default class CharactorCard extends React.Component{
-    render(){
-        return(
-            <div>
-               {this.props.value}
+import React from 'react';
+ 
+class CharacterCard extends React.Component {
+ 
+    state = {
+        active: false
+    }
+ 
+    activate = () => {
+        this.setState({
+            active: !this.state.active
+        });
+ 
+        this.props.activationHandler(this.props.value);
+    }
+ 
+    render() {
+        let activeClass = this.state.active ? 'activeCard' : '';
+        let className = `card ${activeClass}`
+        return (
+            <div className={className} onClick={this.activate}>
+                {this.props.value}
             </div>
         )
     }
 }
+ 
+export default CharacterCard;
